6840947e125ed23557b14e5f68dd6a419f346ceb
Longben <longben@gmail.com>
2012-10-15 19:17:26 +0800

    Sitemap优化

--- app/Plugin/Wczhs/Config/routes.php
+++ app/Plugin/Wczhs/Config/routes.php
@@ -1,6 +1,7 @@
 <?php
     Router::connect('/app/:action/*', array('plugin' => 'wczhs', 'controller' => 'apps'));
 	Router::connect('/', array('plugin'=>'wczhs' ,'controller' => 'apps', 'action' => 'index'));
+	Router::connect('/app', array('plugin'=>'wczhs' ,'controller' => 'apps', 'action' => 'main'));
 	Router::connect('/main', array('plugin'=>'wczhs' ,'controller' => 'apps', 'action' => 'main'));
 	Router::connect('/education', array('plugin'=>'wczhs' ,'controller' => 'apps', 'action' => 'main'));
 	Router::connect('/english', array('plugin'=>'wczhs' ,'controller' => 'apps', 'action' => 'english'));
