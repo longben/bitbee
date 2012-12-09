4996122ea36c55a6c3bad6158736b6f49b17a358
Longben <longben@gmail.com>
2012-12-10 00:52:52 +0800

    .

--- app/Model/Code.php
+++ app/Model/Code.php
@@ -1,6 +1,35 @@
 <?php
 class Code extends AppModel {
-	var $name = 'Code';
-	var $displayField = 'name';
+	public $name = 'Code';
+    
+    public $actsAs = array('Tree');
+
+	public $displayField = 'name';
+
+    function beforeSave($created) {
+
+        if(isset($this->data['Code']['file']) && !empty($this->data['Code']['file'])){
+            extract($this->data['Code']['file']);
+            if ($size > 0 && !$error) {
+
+                App::import('Vendor', '/utils/file');
+
+                $_new_filename = md5(time().$name).'.'.getFileExtension($name);
+                $_tmp_filename = md5(md5(time().$name)).'.'.getFileExtension($name);
+
+                $uploadfile = UPLOAD_PATH. 'images'. DS . $_new_filename;
+
+                move_uploaded_file($tmp_name, $uploadfile);
+
+                $this->data['Code']['icon'] = $_new_filename;
+            }else{
+
+            }
+        }
+
+        return true;
+    }
+
+
 }
 ?>
