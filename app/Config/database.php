ff9420db199bf309ff440f2f5f36c23edfed794f
Longben <longben@gmail.com>
2013-02-27 17:20:33 +0800

    全局DB设置

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,13 +1,19 @@
 <?php
 class DATABASE_CONFIG {
 
-	public $default = array(
-		'datasource' => 'HpMysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'root',
-		'password' => '',
-		'database' => 'hy',
-        'encoding' => 'utf8',
-	);
+
+    public function __construct(){
+        $db_name = is_null(Configure::read('Site.db'))?'hy':Configure::read('Site.db');
+
+        $this->default = array(
+            'datasource' => 'HpMysql',
+            'persistent' => false,
+            'host' => 'localhost',
+            'login' => 'bitbee',
+            'password' => 'M9HcYnVC5fwAdjV5',
+            'database' => $db_name,
+            'encoding' => 'utf8',
+        );
+    }
+
 }
